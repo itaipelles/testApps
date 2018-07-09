f8d723c7adac90a744dd7caa6e753f34e5aee1ae
Alexander Jdanov <alexanderj@wix.com>
2018-07-09 18:30:23 +0300

    Add schema file
    onPublished

--- provisionapp.js
+++ provisionapp.js
@@ -1,21 +1,32 @@
+let _editorSDK;
 module.exports = {
   editorReady: function(editorSDK) {
+    _editorSDK = editorSDK;
+  },
+  onEvent: ({ eventType, eventPayload }) => {
+  if (eventType == 'siteWasPublished') {
     const WIX_CODE_APP_DEF_ID = "675bbcef-18d8-41f5-800e-131ec9e08762";
     console.log("Before Installing WixCode...");
-    editorSDK.document.application
+    _editorSDK.document.application
       .install("token", { appDefinitionId: WIX_CODE_APP_DEF_ID })
       .then(() => console.log("WIXCODE INSTALLED"))
       .then(() =>
-        editorSDK.vfs.writeFile("token", {
-          path: "backend/some-file.js",
-          content: "// js content"
+        _editorSDK.vfs.writeFile("token", {
+          path: "backend/test-file.js",
+          content: "// test me"
         })
       )
+      .then(() =>
+      _editorSDK.vfs.writeFile("token", {
+          path: ".schemas/TestCollection.json",
+          content: '{“id”:“TestCollection”,“displayName”:“TestCollection”,“displayField”:“title”,“fields”:{“title”:{“displayName”:“Title”,“type”:“text”}}}'
+        })
+      )      
       .then(() => console.log("WROTE A BACKEND FILE"))
+      .then(() => console.log("Create collection"))
       .catch(e => console.log("ERROR: ", e));
+  }
   },
-  onEvent: ({ eventType, eventPayload }) => {
-  console.log(eventType)},
   getAppManifest: () => {}
 };
 
